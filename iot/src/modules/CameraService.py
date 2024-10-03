e863da4a350d3f411d073b97aeaced57c1645da1
rej <rej.safranko@codxsolutions.hr>
2024-10-03 10:03:18 +0200

    path, cam module

--- iot/src/modules/CameraService.py
+++ iot/src/modules/CameraService.py
@@ -4,21 +4,20 @@ import numpy
 
 class CameraService:
     def __init__(self):
-        pass
+        self.camera = cv2.VideoCapture(0)
 
     def capture_image(self) -> numpy.ndarray:
         """Captures an image from Raspberry Pi camera using OpenCV."""
-        camera = cv2.VideoCapture(0)
-        ret, frame = camera.read()
+        ret, frame = self.camera.read()
         if not ret:
             print("Failed to capture image.")
             return None
-        camera.release()
+        self.camera.release()
         return frame
 
     def dummy_image(self) -> numpy.ndarray:
         image = cv2.imread(
-            "/home/raspberrypi/Projects/solar/SolarSense/iot/20210916_094136_4_11zon.jpg",
+            "/home/raspberrypi/Projects/solar/SolarSense/iot/Imgdirty_5_1.jpg",
             cv2.IMREAD_COLOR,
         )
         return image
